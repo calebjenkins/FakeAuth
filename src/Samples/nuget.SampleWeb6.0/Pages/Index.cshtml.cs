06c544b0f34661dae4863f8eb83dd69fc6b3bb4a
calebjenkins <caleb@calebjenkins.com>
2021-12-11 22:44:22 -0600

    added .net 6 sample

---
+++ src/Samples/nuget.SampleWeb6.0/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nuget.SampleWeb6._0.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
