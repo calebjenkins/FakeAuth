d9ca5a8d06b1c8a7a6a8d74912fe00dbcba341bc
calebjenkins <caleb@calebjenkins.com>
2022-02-09 23:10:38 -0600

    added Extensions, tests and bumped version

---
+++ src/FakeAuth/Profiles/FakeAuthProfileExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace FakeAuth.Profiles
+{
+	public static class FakeAuthProfileExtension
+	{
+		public static IList<string> GetClaimKeys(this IFakeAuthProfile profile)
+		{
+
+			List<string> keys = new List<string>();
+			var claims = (List < Claim >) profile.GetClaims();
+			
+			claims.ForEach((c) =>
+			{
+				if (!keys.Contains(c.Type))
+				{
+					keys.Add(c.Type);
+				}
+			});
+
+			return keys;
+		}
+
+		public static IList<Claim> GetClaims(this IFakeAuthProfile profile)
+		{
+			var builder = profile.OptionBuilder();
+
+			FakeAuthOptions options = new FakeAuthOptions();
+			profile.OptionBuilder().Invoke(options);
+			return options.Claims;
+		}
+	}
+}
