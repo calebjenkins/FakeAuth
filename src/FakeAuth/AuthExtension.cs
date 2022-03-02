f9beec357fe4b15056f3851bae54a56e3d65d7ca
calebjenkins <caleb@calebjenkins.com>
2022-03-02 13:50:49 -0600

    Seperate out AddFakeAuth from UseFAkeAuth - Add Obsolete attribute to UseFakeAuth

--- src/FakeAuth/AuthExtension.cs
+++ src/FakeAuth/AuthExtension.cs
@@ -7,15 +7,35 @@ namespace FakeAuth
 {
 	public static class AuthExtension
 	{
+
 		public static AuthenticationBuilder AddFakeAuth(this AuthenticationBuilder authbuilder)
 		{
-			authbuilder.Services.UseFakeAuth();
+			return AddFakeAuth<DefaultProfile>(authbuilder);
+		}
+		public static AuthenticationBuilder AddFakeAuth<TProfile>(this AuthenticationBuilder authbuilder) where TProfile : IFakeAuthProfile, new()
+		{
+			IFakeAuthProfile profile = new TProfile();
+			Action<FakeAuthOptions> options = profile.OptionBuilder();
+			
+			return AddFakeAuth(authbuilder, options);
+		}
+
+		public static AuthenticationBuilder AddFakeAuth(this AuthenticationBuilder authbuilder, Action<FakeAuthOptions> options)
+		{
+			authbuilder.Services.AddAuthentication(FakeAuthDefaults.SchemaName)
+				.AddScheme<FakeAuthOptions, FakeAuthHandler>(FakeAuthDefaults.SchemaName, null);
+
+			authbuilder.Services.Configure<FakeAuthOptions>(FakeAuthDefaults.SchemaName, options);
 			return authbuilder;
 		}
+
+		[Obsolete("Please use AddAuthentication().AddFakeAuth() instead") ]
 		public static void UseFakeAuth(this IServiceCollection services)
 		{
 			services.UseFakeAuth<DefaultProfile>();
 		}
+
+		[Obsolete("Please use AddAuthentication().AddFakeAuth() instead")]
 		public static void UseFakeAuth<TProfile>(this IServiceCollection services) where TProfile : IFakeAuthProfile, new()
 		{
 			IFakeAuthProfile profile = new TProfile();
@@ -23,6 +43,7 @@ namespace FakeAuth
 			UseFakeAuth(services, options);
 		}
 
+		[Obsolete("Please use AddAuthentication().AddFakeAuth() instead")]
 		public static void UseFakeAuth(this IServiceCollection services, Action<FakeAuthOptions> options)
 		{
 			services.AddAuthentication(FakeAuthDefaults.SchemaName)
